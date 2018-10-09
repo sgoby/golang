4942fad2190bd0ddfe20d0227ec521730d1895f3
sgoby <sangzhihong@163.com>
2018-10-09 09:01:04 +0800

    Update tostruct.go

--- utils/convert/tostruct.go
+++ utils/convert/tostruct.go
@@ -40,43 +40,59 @@ func InterfaceToStruct(vmap interface{}, mstuct interface{}) (err error) {
 value 复制,带基础类型自动转化功能
 */
 func valueToValue(vmapValue reflect.Value, mstuct reflect.Value) (err error) {
-	vmapValue = getElemValue(vmapValue)
-	mstuct = getElemValue(mstuct)
+	vmapValue = GetElemValue(vmapValue)
+	mstuct = GetElemValue(mstuct)
 	//
-	if vmapValue.Kind() == mstuct.Kind() && isBaseType(mstuct.Kind()) {
-		mstuct.Set(vmapValue)
-	} else if vmapValue.Kind() == mstuct.Kind() && !isBaseType(mstuct.Kind()) {
-		//此处可以跟据需求，自行扩展
-		if mstuct.Kind() == reflect.Slice || mstuct.Kind() == reflect.Array {
-			//
-			elemType := mstuct.Type().Elem()
-			mstuctSliceV := reflect.MakeSlice(mstuct.Type(), 0, 0)
-			//
-			for i := 0; i < vmapValue.Len(); i++ {
-				t := reflect.New(elemType).Elem()
-				m := vmapValue.Index(i)
-				if err := valueToValue(m, t); err != nil {
-					return err
+	if vmapValue.Kind() == mstuct.Kind() {
+		if IsBaseType(mstuct.Kind()) {
+			mstuct.Set(vmapValue)
+		} else {
+			//此处可以跟据需求，自行扩展
+			if mstuct.Kind() == reflect.Slice || mstuct.Kind() == reflect.Array {
+				//
+				elemType := mstuct.Type().Elem()
+				mstuctSliceV := reflect.MakeSlice(mstuct.Type(), 0, 0)
+				//
+				for i := 0; i < vmapValue.Len(); i++ {
+					t := reflect.New(elemType).Elem()
+					m := vmapValue.Index(i)
+
+					if err := valueToValue(m, t); err != nil {
+
+						return err
+					}
+					mstuctSliceV = reflect.Append(mstuctSliceV, t)
 				}
-				mstuctSliceV = reflect.Append(mstuctSliceV, t)
+				//
+				l := mstuct.Len()
+				if l > 0 {
+					reflect.Copy(mstuct,mstuctSliceV.Slice(0,l))
+					return nil
+				}
+				mstuct.Set(mstuctSliceV)
+				return nil
+
+			} else if mstuct.Kind() == reflect.Interface || mstuct.Kind() == reflect.Map {
+				mstuct.Set(vmapValue)
 			}
-			mstuct.Set(mstuctSliceV)
-			return nil
 		}
 		//=========================以下部分是类型不相等的===================================
-	} else if isBaseType(vmapValue.Kind()) && isBaseType(mstuct.Kind()) {
-		nVal, err := valueConvert(vmapValue, mstuct.Type())
+	} else if IsBaseType(vmapValue.Kind()) && IsBaseType(mstuct.Kind()) {
+		nVal, err := ValueConvert(vmapValue, mstuct.Type())
 		if err != nil {
 			return err
 		}
 		mstuct.Set(nVal)
 		//
 	} else if vmapValue.Kind() == reflect.Interface {
+		if vmapValue.IsNil(){
+			return nil
+		}
 		i := vmapValue.Interface()
 		v := reflect.ValueOf(i)
 		//
-		if isBaseType(v.Kind()) {
-			nVal, err := valueConvert(v, mstuct.Type())
+		if IsBaseType(v.Kind()) {
+			nVal, err := ValueConvert(v, mstuct.Type())
 			if err != nil {
 				return err
 			}
@@ -85,26 +101,41 @@ func valueToValue(vmapValue reflect.Value, mstuct reflect.Value) (err error) {
 			if err := valueToValue(v, mstuct); err != nil {
 				return err
 			}
+			if mstuct.Kind() == reflect.Ptr{
+				mstuct = mstuct.Elem()
+			}
 		}
 	} else if vmapValue.Kind() == reflect.Map && mstuct.Kind() == reflect.Struct {
-		slicSF := getStructFields(mstuct)
+		slicSF := GetStructFields(mstuct)
 		for _, sf := range slicSF {
 			mapKeyValue := vmapValue.MapIndex(reflect.ValueOf(sf.Name)) //.Elem();
-			mapKeyValue = getElemValue(mapKeyValue)
+			mapKeyValue = GetElemValue(mapKeyValue)
 			if !mapKeyValue.IsValid() {
 				continue
 			}
 			fieldValue := mstuct.FieldByName(sf.Name)
+			fieldValue = GetElemValue(fieldValue)
 			if fieldValue.IsValid() && fieldValue.CanSet() {
 				if err := valueToValue(mapKeyValue, fieldValue); err != nil {
 					return err
 				}
 			}
 		}
-	} else if mstuct.IsNil() {
+	} else if mstuct.Kind() == reflect.Array {
+		l := mstuct.Len()
+		nVal := mstuct.Slice(0,l)
+		err := valueToValue(vmapValue,nVal)
+		if err != nil {
+			return err
+		}
+		reflect.Copy(mstuct,nVal.Slice(0,l))
+	} else if  mstuct.IsNil() {
 		mstuct.Set(reflect.New(mstuct.Type().Elem()))
 		if err := valueToValue(vmapValue, mstuct); err != nil {
 			return err
+		}
+		if mstuct.Kind() == reflect.Ptr{
+			mstuct = mstuct.Elem()
 		}
 	} else {
 		//此处可以跟据需求，自行扩展
@@ -114,7 +145,7 @@ func valueToValue(vmapValue reflect.Value, mstuct reflect.Value) (err error) {
 }
 
 //
-func valueConvert(v reflect.Value, t reflect.Type) (newVal reflect.Value, err error) {
+func ValueConvert(v reflect.Value, t reflect.Type) (newVal reflect.Value, err error) {
 	defer func() {
 		if panidErr := recover(); panidErr != nil {
 			err = fmt.Errorf("'%v' %v", v, panidErr)
@@ -126,7 +157,7 @@ func valueConvert(v reflect.Value, t reflect.Type) (newVal reflect.Value, err er
 /*
 判断是否为基础类型,跟据需要可自行扩展
 */
-func isBaseType(k reflect.Kind) bool {
+func IsBaseType(k reflect.Kind) bool {
 	for _, vk := range specialType {
 		if k == vk {
 			return false
@@ -138,7 +169,7 @@ func isBaseType(k reflect.Kind) bool {
 /*
 找到有效value
 */
-func getElemValue(v reflect.Value) reflect.Value {
+func GetElemValue(v reflect.Value) reflect.Value {
 	for { //找到内容
 		if v.Kind() != reflect.Ptr || v.IsNil() {
 			break
@@ -149,9 +180,22 @@ func getElemValue(v reflect.Value) reflect.Value {
 }
 
 /*
+找到有效value
+*/
+func GetElemType(v reflect.Type) reflect.Type {
+	for { //找到内容
+		if v.Kind() != reflect.Ptr || IsBaseType(v.Kind()) {
+			break
+		}
+		v = v.Elem()
+	}
+	return v
+}
+
+/*
 获取Struct 的所有字段
 */
-func getStructFields(val reflect.Value) []reflect.StructField {
+func GetStructFields(val reflect.Value) []reflect.StructField {
 	pType := val.Type()
 	if pType == nil {
 		return nil
